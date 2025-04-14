c0b34e781a9153637c465cd572d2f143122f09c4
Brankica Stefanovic <129560295+InityAgency@users.noreply.github.com>
2025-04-15 01:03:47 +0200

    feat(web) : Single Brand Newsletter block

--- apps/web/src/app/(pages)/brands/[id]/page.tsx
+++ apps/web/src/app/(pages)/brands/[id]/page.tsx
@@ -5,6 +5,7 @@ import { useParams } from "next/navigation";
 import { Brand } from "@/types/brand";
 import { BrandTitleSkeleton } from "@/components/brands/BrandTitleSkeleton";
 import Image from "next/image";
+import NewsletterBlock from "@/components/web/Newsletter/NewsletterBlock";
 export default function BrandPage() {
     const [brand, setBrand] = useState<Brand | null>(null);
     const [loading, setLoading] = useState(true);
@@ -36,14 +37,17 @@ export default function BrandPage() {
     }
 
     return (
-        <div className="flex flex-col items-center rounded-b-xl bg-secondary max-w-[calc(100svw-3rem)] 2xl:max-w-[90svw] mx-auto px-4 lg:px-12 py-12 gap-4 xl:gap-8 mb-12">
-            <div className="page-header flex flex-col gap-6 w-full">
-                <div className="flex flex-row gap-4 items-center justify-center rounded-xl mx-auto bg-black/10 p-4 mb-6">
-                    <Image src={`${process.env.NEXT_PUBLIC_API_URL}/api/${process.env.NEXT_PUBLIC_API_VERSION}/media/${brand.logo.id}/content`} alt={brand.name} width={150} height={150}  />
+        <>
+            <div className="flex flex-col items-center rounded-b-xl bg-secondary max-w-[calc(100svw-3rem)] 2xl:max-w-[90svw] mx-auto px-4 lg:px-12 py-12 gap-4 xl:gap-8 mb-12">
+                <div className="page-header flex flex-col gap-6 w-full">
+                    <div className="flex flex-row gap-4 items-center justify-center rounded-xl mx-auto bg-black/10 p-4 mb-6">
+                        <Image src={`${process.env.NEXT_PUBLIC_API_URL}/api/${process.env.NEXT_PUBLIC_API_VERSION}/media/${brand.logo.id}/content`} alt={brand.name} width={150} height={150}  />
+                    </div>
+                    <h1 className="text-5xl font-bold text-center">{brand.name}</h1>
+                    <p className="text-center text-xl max-w-2xl mx-auto">{brand.description}</p>
                 </div>
-                <h1 className="text-5xl font-bold text-center">{brand.name}</h1>
-                <p className="text-center text-xl max-w-2xl mx-auto">{brand.description}</p>
             </div>
-        </div>
+            <NewsletterBlock />
+        </>
     );
 }
