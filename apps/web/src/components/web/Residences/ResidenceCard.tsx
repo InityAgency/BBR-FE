bcb0730770e51440e7d5f6009f4f5b508ee65e80
Brankica Stefanovic <129560295+InityAgency@users.noreply.github.com>
2025-04-16 01:11:53 +0200

    fiex(web) : Brands

--- apps/web/src/components/web/Residences/ResidenceCard.tsx
+++ apps/web/src/components/web/Residences/ResidenceCard.tsx
@@ -8,20 +8,21 @@ export function ResidenceCard({ residence }: { residence: Residence }) {
     return (
         <Link href={`/residences/${residence.id}`} className="border p-4 bg-secondary/30 rounded-lg group flex justify-between flex-col not-prose gap-8 hover:bg-zinc-800/20 transition-all h-full hover:-translate-y-2">
             <div className="h-72 w-full overflow-hidden relative rounded-md border flex items-center justify-center">
-                {featuredImage ? (
+                {typeof featuredImage === "object" && featuredImage !== null && "id" in featuredImage ? (
                     <Image
-                    src={`${process.env.NEXT_PUBLIC_API_URL}/api/${process.env.NEXT_PUBLIC_API_VERSION}/media/${residence.featuredImage}/content`}
-                    alt={residence.name}
-                    fill
-                    className="object-cover"
-                />
+                        src={`${process.env.NEXT_PUBLIC_API_URL}/api/${process.env.NEXT_PUBLIC_API_VERSION}/media/${(featuredImage as { id: string }).id}/content`}
+                        alt={residence.name}
+                        fill
+                        className="object-cover"
+                    />
+                ) : null}
                 ) : (
                     <div className="h-72 w-full overflow-hidden relative rounded-md border flex items-center justify-center animate-pulse " />
-                )}
+                ){"}"}
             </div>
             <div className="px-2 pb-4">
                 <h3 className="text-xl font-bold mb-2">{residence.name}</h3>
-                <p className="text-sm text-muted-foreground">{residence.description} </p>
+                <p className="text-md text-muted-foreground">{residence.description} </p>
             </div>
         </Link>
     );
