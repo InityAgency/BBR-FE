0b0d9691c7f8cbcf363232cee1d26ae4ad09c49f
Brankica Stefanovic <129560295+InityAgency@users.noreply.github.com>
2025-05-28 19:19:29 +0200

    fix(web) : Single Unit Unter Construction

---
+++ apps/web/src/app/(pages)/exclusive-deals/[slug]/page.tsx
@@ -0,0 +1,10 @@
+import SectionLayout from "@/components/web/SectionLayout";
+import UnderConstruction from "@/components/web/UnderConstruction";
+
+export default function SingleUnit() {
+    return (
+        <SectionLayout className="w-full">
+            <UnderConstruction />
+        </SectionLayout>
+    );
+}
